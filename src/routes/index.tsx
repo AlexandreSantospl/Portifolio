9d8d74e6313aa52c8645c9c8e9e56675e66b6af9
Alexandre Santos <163349786+AlexandreSantospl@users.noreply.github.com>
2024-07-24 18:36:38 -0300

    erro de url

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -20,9 +20,9 @@ export const AppRoutes = () => {
     return (
         <Routes>
 
-            <Route path="/pagina-inicial" element={<PaginaInicial/>} />
+            <Route path="/portifolio" element={<PaginaInicial/>} />
             
-            <Route path="*" element={<Navigate to="pagina-inicial" />} />
+            <Route path="*" element={<Navigate to="portifolio" />} />
 
         </Routes>
     )
